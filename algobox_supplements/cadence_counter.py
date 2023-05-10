eb3a674ffcc7c817198debdb6afb92711d2df1e5
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-05-10 10:21:56 -0400

    counters

--- algobox_supplements/cadence_counter.py
+++ algobox_supplements/cadence_counter.py
@@ -4,30 +4,29 @@ class CounterApp:
     def __init__(self, root):
         self.counter = 0
         self.root = root
-        self.root.geometry('200x100')
+        self.root.geometry('500x200')
         self.root.title('CADENCE COUNTER')
         self.root.configure(bg='black')
-        self.root.resizable(0, 0)
 
         # counter display
-        self.counter_disp = tk.Label(root, text=str(self.counter), bg='black', fg='white')
-        self.counter_disp.pack(side=tk.LEFT)
+        self.counter_disp = tk.Label(root, text=str(self.counter), bg='black', fg='white', font=("", 45))
+        self.counter_disp.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         # frame for buttons
         button_frame = tk.Frame(root, bg='black')
-        button_frame.pack(side=tk.RIGHT)
+        button_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
         # increment button
-        self.inc_button = tk.Button(button_frame, text='+', command=self.increment, fg='black', bg='white')
-        self.inc_button.pack()
+        self.inc_button = tk.Button(button_frame, text='+', command=self.increment, fg='black', bg='white', font=("", 45))
+        self.inc_button.pack(fill=tk.BOTH, expand=True)
 
         # clear button
-        self.clear_button = tk.Button(button_frame, text='clear', command=self.clear, fg='black', bg='white')
-        self.clear_button.pack()
+        self.clear_button = tk.Button(button_frame, text='clear', command=self.clear, fg='black', bg='white', font=("", 45))
+        self.clear_button.pack(fill=tk.BOTH, expand=True)
 
         # decrement button
-        self.dec_button = tk.Button(button_frame, text='-', command=self.decrement, fg='black', bg='white')
-        self.dec_button.pack()
+        self.dec_button = tk.Button(button_frame, text='-', command=self.decrement, fg='black', bg='white', font=("", 45))
+        self.dec_button.pack(fill=tk.BOTH, expand=True)
 
     def increment(self):
         self.counter += 1
